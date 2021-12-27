7682c83187b9b830d6f6361cafbc4f392667de91
romanshliakhov <romanshliakhov@gmail.com>
2021-12-27 14:57:46 +0200

    starting calculator

--- source/js/common.js
+++ source/js/common.js
@@ -23,8 +23,6 @@ let swiperReady = new Swiper('.ready__slider', {
   },
 });
 
-
-
 // imput Mask
 let selector = document.querySelectorAll('input[type="tel"]');
 let im = new Inputmask('+9 (999) 999-99-99');
@@ -58,3 +56,77 @@ let validateForms = function(selector, rules, successModal, yaGoal) {
 };
 
 validateForms('.form', { tel: {required: true} }, '.thanks-popup', 'send goal');
+
+
+// Калькулятор выбранных товаров
+
+// let chooseItems = document.querySelector('#choose');
+// let totalPrice = document.querySelector('#total');
+
+// let itemsPrice = [...chooseItems.querySelectorAll('.collect__item')];
+// // console.log("itemsPrice :>> ", itemsPrice);
+
+// itemsPrice.forEach((el) => {
+//   // console.log("el :>> ", el);
+
+//   el.addEventListener('click', function () {
+//     if (this.classList.contains('active')) {
+//       this.classList.remove('active');
+//     } else {
+//       this.classList.add('active');
+//     }
+
+//     let activeItems = getActiveItems(chooseItems);
+//     let sum = sumPrice(activeItems);
+//     showTotalSum(sum, totalPrice);
+//   });
+// });
+
+// function getActiveItems (chooseItems) {
+//   return [...chooseItems.querySelectorAll('.collect__item.active')];
+// }
+
+// function sumPrice (items) {
+//   let sum = 0;
+
+//   for ( let i = 0; i < items.lenght; i++) {
+//     sum += Number(items[i].innerHTML);
+//   }
+
+//   return sum;
+// }
+
+// function showTotalSum (sum, elem) {
+//   elem.innerHTML = sum;
+// }
+
+
+const calc = document.querySelector('.calc');
+const result = document.querySelector('#result');
+
+calc.addEventListener('click', function (event) {
+  if (!event.target.classList.contains('calc__btn')) return;
+
+  // const value = this.getAttribute("data-value");
+
+  const value = event.target.innerText;
+  // const value = this.target.dataset;
+
+  // switch(value) {
+  //   case 'C':
+  //     result.innerText = '';
+  //     break;
+
+  //   case '=':
+  //     result.innerText = eval(result.innerText);
+  //     break;
+
+  //   default:
+  //     result.innerText += value ;
+  // }
+
+
+  console.log(value);
+
+  result.innerText = value;
+});
